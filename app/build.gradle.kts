9412c17ee49eb2b86414ea15d0170a0da08f9d62
Barak Holtzer <barakeholtzer@gmail.com>
2026-05-26 11:38:04 +0300

    update version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
         applicationId = "com.bih.mangosos"
         minSdk = 28
         targetSdk = 35
-        versionCode = 5
-        versionName = "5"
+        versionCode = 7
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
